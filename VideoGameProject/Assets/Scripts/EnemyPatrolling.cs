4f5ebc94b1c93752b94bc6cbda7501ebf9e7075b
samosunaz <a01630427@itesm.mx>
2016-09-04 14:15:52 -0500

    Added new assets
    Enemies, materials and Scripts have been added

---
+++ VideoGameProject/Assets/Scripts/EnemyPatrolling.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyPatrolling : MonoBehaviour {
+
+	public Transform[] checkpointsArr;
+	private int currentCheckpoint;
+	public float treshold;
+	public int speed;
+
+	// Use this for initialization
+	void Start () {
+		this.currentCheckpoint = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.LookAt (this.checkpointsArr[this.currentCheckpoint]);
+		transform.Translate (transform.forward * Time.deltaTime * this.speed, Space.World);
+		float distance = Vector3.Distance (transform.position, this.checkpointsArr[this.currentCheckpoint].position);
+		if (distance < this.treshold) {
+			this.currentCheckpoint++;
+			if (this.currentCheckpoint == this.checkpointsArr.Length) {
+				this.currentCheckpoint = 0;
+			}
+		}
+	}
+}
---
+++ VideoGameProject/Assets/Scripts/Node.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Node : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnDrawGizmos () {
+		Gizmos.color = Color.gray;
+		Gizmos.DrawSphere (transform.position, 1);
+	}
+
+	void OnDrawGizmosSelected () {
+		Gizmos.color = Color.blue;
+		Gizmos.DrawSphere (transform.position, 1);
+	}
+
+}
