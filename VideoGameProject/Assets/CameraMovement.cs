fa3ae26112b180a0c31a99626b0bbccfd98d395a
Kevin <kevintroko@gmail.com>
2016-09-04 17:19:32 -0500

    PlayerMovement

---
+++ VideoGameProject/Assets/CameraMovement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+	private Vector3 offset;
+	public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.transform.position;
+	}
+
+	// Update is called once per frame
+	void LateUpdate () {
+		transform.position = player.transform.position + offset;
+	}
+}
---
+++ VideoGameProject/Assets/Scripts/MovementPlayer.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MovementPlayer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float h = Input.GetAxis ("Horizontal");
+		float v = Input.GetAxis ("Vertical");
+		transform.Translate (h * 10 * Time.deltaTime, 0, v * 10 * Time.deltaTime, Space.World);
+
+	}
+
+	void OnCollisionEnter(Collision c){
+		Debug.Log("Entered Collition with: " + c.transform.name);
+		if (c.gameObject.CompareTag ("Enemy")){
+			SceneManager.LoadScene ("BattleScene");
+		}
+
+		//If you want an enemy to disappear, use this code
+		/*if (c.gameObject.CompareTag ("Enemy")){
+			c.gameObject.SetActive (false);
+		}*/
+	}
+}
+	
