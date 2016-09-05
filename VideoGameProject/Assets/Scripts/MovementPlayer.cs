1b717de5ebdff83957a2fe7b80490004cefabe67
samosunaz <a01630427@itesm.mx>
2016-09-04 19:43:45 -0500

    Player's position
    Fixed Player's position after beating an enemy

--- VideoGameProject/Assets/Scripts/MovementPlayer.cs
+++ VideoGameProject/Assets/Scripts/MovementPlayer.cs
@@ -4,29 +4,47 @@ using UnityEngine.SceneManagement;
 
 public class MovementPlayer : MonoBehaviour {
 
+	public int speed;
+	private static bool sceneSwitched;
+
+	private GameObject thePlayer;
+
 	// Use this for initialization
 	void Start () {
-	
+		thePlayer = GameObject.Find("Player");
+		if (sceneSwitched) {
+			PlayerComingBack ();
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		float h = Input.GetAxis ("Horizontal");
 		float v = Input.GetAxis ("Vertical");
-		transform.Translate (h * 10 * Time.deltaTime, 0, v * 10 * Time.deltaTime, Space.World);
+		transform.Translate (h * speed * Time.deltaTime, 0, v * speed * Time.deltaTime, Space.World);
 
 	}
 
 	void OnCollisionEnter(Collision c){
-		Debug.Log("Entered Collition with: " + c.transform.name);
-		if (c.gameObject.CompareTag ("Enemy")){
+		Debug.Log("Entered Collision with: " + c.transform.name);
+		if (c.gameObject.CompareTag ("Enemy")) {
+			sceneSwitched = true;
+			PlayerSwitchingScene ();
 			SceneManager.LoadScene ("BattleScene");
 		}
+	}
 
-		//If you want an enemy to disappear, use this code
-		/*if (c.gameObject.CompareTag ("Enemy")){
-			c.gameObject.SetActive (false);
-		}*/
+	void PlayerSwitchingScene () {
+		PlayerPrefs.SetFloat ("PlayerX", thePlayer.transform.position.x);
+		PlayerPrefs.SetFloat ("PlayerY", thePlayer.transform.position.y);
+		PlayerPrefs.SetFloat ("PlayerZ", thePlayer.transform.position.z);
+	}
+
+	void PlayerComingBack () {
+		float newPlayerX = PlayerPrefs.GetFloat ("PlayerX");
+		float newPlayerY = PlayerPrefs.GetFloat ("PlayerY");
+		float newPlayerZ = PlayerPrefs.GetFloat ("PlayerZ");
+		thePlayer.transform.position = new Vector3 (newPlayerX, newPlayerY, newPlayerZ);
 	}
 }
 	
---
+++ VideoGameProject/Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerStats : MonoBehaviour {
+
+	public int playerHP;
+	public int playerStrength;
+
+	// Use this for initialization
+	void Start () {
+		PlayerPrefs.SetInt ("PlayerHP", playerHP);
+		PlayerPrefs.SetInt ("PlayerStrenght", playerStrength);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
