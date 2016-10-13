3ac76331552fb24bb0257b1c7e9d9b15ada07264
Kevin <kevintroko@gmail.com>
2016-10-12 19:38:42 -0500

    scripts related to the map moved into a carpet

---
+++ VideoGameProject/Assets/Scripts/World Related/CoinRotation.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoinRotation : MonoBehaviour {
+
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+        if (speed == 0) {
+            speed = 1;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (new Vector3 (60 * speed, 0, 0) * Time.deltaTime); 
+	}
+}
---
+++ VideoGameProject/Assets/Scripts/World Related/UIBehaviours.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class UIBehaviours : MonoBehaviour {
+
+    public string sceneName;
+
+	public void HeyGuyzButton() {
+		SceneManager.LoadScene (sceneName);
+	}
+}
